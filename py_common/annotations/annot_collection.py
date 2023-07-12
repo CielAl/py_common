5c4547b7d951fc07e5ecc67c3b8fddfc8f18b69e
CielAl <cielmercy@gmail.com>
2023-07-11 22:04:27 -0400

    label assignment

--- py_common/annotations/annot_collection.py
+++ py_common/annotations/annot_collection.py
@@ -1,10 +1,9 @@
-from typing import List, Dict, Union, Type, Literal, get_args, Tuple
-from shapely.strtree import STRtree
+from typing import List, Dict, Union, Type, Literal, get_args, Tuple, Mapping
+from types import MappingProxyType
+# from shapely.strtree import STRtree
+# from shapely.geometry import box as shapely_box
 from lazy_property import LazyProperty
-from shapely.geometry import box as shapely_box
-from shapely.strtree import STRtree
-
-from .annotation.base import Annotation, Region
+from .annotation.base import Annotation, Region, TYPE_RAW_LABEL
 from .annotation.imagescope import ImageScopeAnnotation
 from .annotation.geojson import GEOJsonAnnotation
 
@@ -22,6 +21,7 @@ PARSER_BUILDER_MAP: Dict[str, Type[Annotation]] = {
 
 class AnnotCollection:
     _annotation_list: List[Annotation]
+    _label_to_regions_map: Mapping[TYPE_RAW_LABEL, List[Region]]
 
     @LazyProperty
     def all_regions(self) -> List[Region]:
@@ -30,17 +30,29 @@ class AnnotCollection:
             region_list += annotation.regions
         return region_list
 
-    @LazyProperty
-    def str_tree(self) -> STRtree:
-        regions: List[Region] = self.all_regions
-        polygons = [r['polygon'] for r in regions]
-        return STRtree(polygons)
+    # @LazyProperty
+    # def multipolygons(self) -> MultiPolygon:
+    #     for annotation in self._annotation_list:
 
     def __init__(self, annotation_list: List[Annotation]):
         self._annotation_list = annotation_list
+        self._label_to_regions_map = self._new_label_to_regions_map()
 
     @classmethod
-    def build(cls, parser_type: TYPE_SUPPORTED_PARSER, uri: str, label_map: Dict[Union[str, int], int]):
+    def build(cls, parser_type: TYPE_SUPPORTED_PARSER, uri: str, label_map: Union[Dict[Union[str, int], int], None]):
         construct = PARSER_BUILDER_MAP[parser_type]
         annotation_list = construct.build_from_uri(uri=uri, label_map=label_map)
         return cls(annotation_list)
+
+    def _new_label_to_regions_map(self) -> Mapping[TYPE_RAW_LABEL, List[Region]]:
+        out_dict: Dict[TYPE_RAW_LABEL, List[Region]] = dict()
+        for region in self.all_regions:
+            region: Region
+            label: TYPE_RAW_LABEL = region['label']
+            out_dict[label] = out_dict.get(label, [])
+            out_dict[label].append(region)
+        return MappingProxyType(out_dict)
+
+    @property
+    def label_to_regions_map(self) -> Mapping[TYPE_RAW_LABEL, List[Region]]:
+        return self._label_to_regions_map
--- py_common/annotations/labeling.py
+++ py_common/annotations/labeling.py
@@ -1,5 +1,6 @@
 from .annot_collection import AnnotCollection
-from typing import Tuple, List
+from typing import Tuple, List, Union, Dict
+from .annotation.base import TYPE_RAW_LABEL, Region
 from shapely.geometry import box as shapely_box
 from shapely.geometry import Polygon
 from shapely.strtree import STRtree
@@ -11,6 +12,8 @@ class Labeler:
     _tiles: List[Polygon]
     _annot_collection: AnnotCollection
     _tile_tree: STRtree
+
+    BG_LABEL: TYPE_RAW_LABEL = 0
 
     @staticmethod
     def _bbox_sanitized(bbox: TYPE_BBOX):
@@ -34,8 +37,70 @@ class Labeler:
     def annot_collection(self):
         return self._annot_collection
 
+    @staticmethod
+    def valid_box(bbox: TYPE_BBOX) -> Polygon:
+        bbox_polygon = shapely_box(*bbox)
+        if bbox_polygon.is_valid:
+            return bbox_polygon
+        return bbox_polygon.buffer(0)
+
     def __init__(self, bbox_list: List[TYPE_BBOX], annot_collection: AnnotCollection):
         self._bbox_list = [Labeler._bbox_sanitized(bbox) for bbox in bbox_list]
-        self._tiles = [shapely_box(*bbox) for bbox in bbox_list]
+        self._tiles = [Labeler.valid_box(*bbox) for bbox in bbox_list]
         self._annot_collection = annot_collection
         self._tile_tree = STRtree(self._tiles)
+
+    @staticmethod
+    def _index_threshold(tile_list: List[Polygon], region: Region, raw_indices: List[int],
+                         threshold: Union[float, None]) -> List[int]:
+        region_polygon = region['polygon']
+        if threshold is None or threshold <= 0:
+            return [x for x in raw_indices]
+        # for tile in tile_list:
+        #     intersect: Polygon = tile.intersection(region_polygon)
+        return [index for index in raw_indices if tile_list[index].intersection(region_polygon).area > 0]
+
+    @staticmethod
+    def query_tiles_in_region(str_tree: STRtree, region: Region,
+                              threshold: float = None, predicate='overlaps', distance=None):
+        # label = region['label']
+        polygon = region['polygon']
+        raw_index = str_tree.query(polygon, predicate=predicate, distance=distance)
+        all_index_in_annotation = [x for x in raw_index]
+        index_threshed = Labeler._index_threshold(str_tree.geometries, region, raw_index, threshold)
+        return index_threshed, all_index_in_annotation
+
+    @staticmethod
+    def query_tiles_by_label(str_tree: STRtree, annot_collection: AnnotCollection, label: TYPE_RAW_LABEL,
+                             threshold: float = None, predicate='overlaps', distance=None):
+        region_list: List[Region] = annot_collection.label_to_regions_map[label]
+        index_threshed = []
+        all_index_in_annotation = []
+        for region in region_list:
+            curr_threshed, curr_all_in_region = Labeler.query_tiles_in_region(str_tree, region, threshold,
+                                                                              predicate, distance)
+            index_threshed += curr_threshed
+            all_index_in_annotation += curr_all_in_region
+        return index_threshed, all_index_in_annotation
+
+    @staticmethod
+    def _new_index_dict(keys: List[TYPE_RAW_LABEL]) -> Dict:
+        o_dict = dict()
+        for k in keys:
+            o_dict[k] = []
+        return o_dict
+
+    def query_tiles(self, threshold: float = None, predicate='overlaps', distance=None):
+        all_keys = [Labeler.BG_LABEL] + list(self.annot_collection.label_to_regions_map.keys())
+        index_threshed: Dict = Labeler._new_index_dict(all_keys)
+        all_index_in_annotation = []
+        for label in self.annot_collection.label_to_regions_map:
+            index_threshed_label, in_region_label = Labeler.query_tiles_by_label(self._tile_tree,
+                                                                                 self.annot_collection, label,
+                                                                                 threshold, predicate, distance)
+            # index_threshed[label] = index_threshed.get(label, [])
+            index_threshed[label].append()
+            all_index_in_annotation += in_region_label
+        # index_threshed[Labeler.BG_LABEL] = index_threshed.get(Labeler.BG_LABEL, [])
+        index_threshed[Labeler.BG_LABEL] += all_index_in_annotation
+        return index_threshed
